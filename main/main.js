4c4e3db72b7fc9922e988d04d2c07ee2de126f7a
KYLE\Surface <kyle.public10415@gmail.com>
2018-11-19 23:29:32 +0800

    finished task

--- main/main.js
+++ main/main.js
@@ -1,4 +1,35 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
-};
+
+
+module.exports = function printInventory(inputs){
+	let result = '***<没钱赚商店>购物清单***\n';
+	let count=0;
+	let total=0;
+	let tmp = [];
+	let lastIndex;
+
+	for(let i = 0 ; i<inputs.length;i++){
+
+		if(!tmp.includes(inputs[i].barcode)){
+			tmp.push(inputs[i].barcode);
+		}
+
+		
+	}
+	for(let i = 0 ; i<tmp.length; i++){
+		for(let j = 0 ; j<inputs.length ; j++){
+			if(tmp[i]===(inputs[j].barcode)){
+				count++;
+				lastIndex = j
+			}
+		}
+		result += '名称：'+inputs[lastIndex].name+'，数量：'+count+inputs[lastIndex].unit+'，单价：'+inputs[lastIndex].price.toFixed(2)+'(元)，小计：'+(count*inputs[lastIndex].price).toFixed(2)+'(元)\n';
+		total +=count*inputs[lastIndex].price;
+		count=0;
+
+
+	}
+	result +='----------------------\n' +
+					 '总计：'+total.toFixed(2)+'(元)\n' +
+					 '**********************';
+	return result;
+}
